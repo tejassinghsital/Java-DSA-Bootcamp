84df10f8fcf6b26b5f2c0f42678f3b5c4bb6c478
Tejas <tejassinghsital@gmail.com>
2021-09-20 16:56:48 +0530

    BAsic L4 course practicals before starting intellij project

---
+++ Course practicals/L4/src/P2CdlArgs.java
@@ -0,0 +1,13 @@
+public class P2CdlArgs{
+	public static void main(String[] args)
+	{
+		System.out.println(args[0]);	
+	}
+}
+
+class P2CdlArgs1{
+	public static void main(String[] args)
+	{
+		System.out.println(args[1]);
+	}
+}
